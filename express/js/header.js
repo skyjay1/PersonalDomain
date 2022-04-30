677f585cc0d305448a68c3c1599b1e8e0c62fa4a
Skyler James <15920401+skyjay1@users.noreply.github.com>
2022-04-29 21:29:51 -0600

    Update image

--- express/js/header.js
+++ express/js/header.js
@@ -1,7 +1,7 @@
 // insert the navigation bar in the header
 document.write(`
 <header>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar fixed-top navbar-expand-sm navbar-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="https://skyjay.me" target="_self">
                 <img src="img/skyjay1-logo1-round-alpha.png" alt="" width="40" height="40">
@@ -12,13 +12,15 @@ document.write(`
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-sm-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#intro" target="_self">About Me</a>
+                        <a class="nav-link" aria-current="page" href="#intro" target="_self">About Me</a>
                     </li>
+                    <li class="navbar-text d-none d-sm-block">&bullet;</li>
                     <li class="nav-item">
                         <a class="nav-link" href="#projects" target="_self">Projects</a>
                     </li>
+                    <li class="navbar-text d-none d-sm-block">&bullet;</li>
                     <li class="nav-item">
                         <a class="nav-link" href="#contact" target="_self">Contact Me</a>
                     </li>
